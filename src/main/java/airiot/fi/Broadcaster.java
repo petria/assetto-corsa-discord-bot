540bb1feb367650213d7fb91aad3295e49046ace
Petri Airio <petri.j.airio@gmail.com>
2017-03-21 15:02:56 +0200

    Add Broadcaster

--- src/main/java/airiot/fi/Broadcaster.java
+++ src/main/java/airiot/fi/Broadcaster.java
@@ -19,13 +19,17 @@ public class Broadcaster {
         if (args.length != 1) {
             System.exit(-1);
         }
+
+        int UDP_PLUGIN_ADDRESS = 12000;    // server_config.ini: UDP_PLUGIN_ADDRESS :port
+        int UDP_PLUGIN_LOCAL_PORT = 11000; // server_config.ini: UDP_PLUGIN_LOCAL_PORT
+
         DatagramSocket socket = null;
         try {
-            socket = new DatagramSocket(12000);
+            socket = new DatagramSocket(UDP_PLUGIN_ADDRESS);
             InetAddress host = InetAddress.getByName("localhost");
             byte data[] = createBroadCastPacket(args[0]);
-            int forwardToPort = 11000;
-            DatagramPacket packet = new DatagramPacket(data, data.length, host, forwardToPort);
+
+            DatagramPacket packet = new DatagramPacket(data, data.length, host, UDP_PLUGIN_LOCAL_PORT);
             socket.send(packet);
 
         } catch (Exception e) {
